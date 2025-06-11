abca1bf4c790f8cd4e5382e8273429edae686ec2
SivaChiyyedu <69842134+SivaChiyyedu@users.noreply.github.com>
2025-06-11 12:29:00 +0530

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -4,43 +4,111 @@
 // import memberIcon from "../assets/user.png";
 // import jaggery from "../assets/Jaggery.png";
 import productsImage from "../assets/productsImage.png";
-import { FaHeart, FaShoppingCart, FaUser, FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaTwitter,
+  FaFacebookF,
+  FaInstagram,
+  FaPinterestP,
+} from "react-icons/fa";
 
 export default function Header() {
   return (
     <>
       <div className="header-main-div">
         <div className="header-wrapper">
-          <div>
-            <button>
-              <FaBars />
-            </button>
-          </div>
-          <div className="org-name">
-            <div>
-              <h2>NIRVIKARA</h2>
+          <div className="contact-wrapper">
+            <div className="mail-wrapper">
+              <p>
+                <span className="icon">
+                  <FaEnvelope />
+                </span>
+                <span>example@example.com</span>
+              </p>
+            </div>
+            <div className="address-wrapper">
+              <p>
+                <span className="icon">
+                  <FaMapMarkerAlt />
+                </span>
+                <span>1st Floor, New World</span>
+              </p>
+            </div>
+            <div className="phone-wrapper">
+              <p>
+                <span className="icon">
+                  <FaPhone />
+                </span>
+                <span>+91 0000000000</span>
+              </p>
             </div>
           </div>
-          <div className="header-wrapper">
-            <div>
-              <button>
-                <FaUser />
-              </button>
-            </div>
-            <div>
-              <button>
-                <FaHeart />
-              </button>
-            </div>
-            <div>
-              <button>
-                <FaShoppingCart />
-              </button>
+          <div className="follow-us-wrapper">
+            <p>
+              <span>Follow Us</span>
+              <span className="icon">
+                <FaTwitter />
+              </span>
+              <span className="icon">
+                <FaFacebookF />
+              </span>
+              <span className="icon">
+                <FaInstagram />
+              </span>
+              <span className="icon">
+                <FaPinterestP />
+              </span>
+            </p>
+          </div>
+          <div className="get-a-quote-wrapper">
+            <div className="get-a-quote">
+              <span>GET A QUOTE</span>
             </div>
           </div>
         </div>
-        <div>
-          <input type="text" placeholder="Search For Ghee" name="search" />
+        <div className="navigation-bar">
+          <div className="logo-wrapper">
+            <img
+              className="logo"
+              src="https://www.gheewala.com/wp-content/uploads/2020/08/logo.png"
+              alt="logo"
+            />
+            <span className="navigation-item">
+              <Link to="/">Nirvikara</Link>
+            </span>
+          </div>
+          <div className="navigation-wrapper">
+            <ul className="navigation-list">
+              <li className="navigation-item">
+                <Link to="/home">HOME</Link>
+              </li>
+              <li className="navigation-item">
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li className="navigation-item">
+                <Link to="/service">SERVICE</Link>
+              </li>
+              <li className="navigation-item">
+                <Link to="/gallery">GALLERY</Link>
+              </li>
+              <li className="navigation-item">
+                <Link to="/blog">BLOG</Link>
+              </li>
+              <li className="navigation-item">
+                <Link to="/contact">CONTACT</Link>
+              </li>
+              <li className="navigation-item">
+                <Link to="/search">
+                  <FaSearch />
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* <input type="text" placeholder="Search For Ghee" name="search" /> */}
         </div>
       </div>
       <div>
---
+++ pages/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our store! We are dedicated to providing you with the best
+        products and services. Our team is passionate about quality and customer
+        satisfaction.
+      </p>
+      <p>
+        Our mission is to offer a wide range of products that meet your needs
+        and exceed your expectations. Thank you for choosing us!
+      </p>
+    </div>
+  );
+}
