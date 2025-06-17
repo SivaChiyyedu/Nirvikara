c4e2288fd36a7102aa88e8a15f2d22eaa32e5efe
JeevanAvinashB <jeevanavinashb@gmail.com>
2025-06-18 00:18:17 +0530

    categories related task

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,4 +1,7 @@
-
+import vegetables from '../../assets/vegetables.jpg';
+import fruits from '../../assets/Fruits.jpg';
+import nuts from '../../assets/Nuts.jpg';
+import './Categories.css'
 
 
 
@@ -7,15 +10,32 @@ function Categories() {
     return (
         <>
         <div className="categories-wrapper">
+            <div className="category-heading-div">
+                 <div>
+            <h2 >Category</h2>
+           </div>
+                <div className='category-view'>
+                    <h4>View All</h4>
+                    <h4>L</h4>
+                    <h4>G</h4>
+                </div>
+            </div>
+          
+                
+           
+            
             <div className="categories-container">
                 <div className="category">
+                    <img src={vegetables} alt="category-vegetable" />
                     <h5>Vegetables</h5>
                 </div>
                 <div className="category">
+                    <img src={fruits} alt="category-fruits" />
                     <h5>Fruits</h5>
                     
                 </div>
                 <div className="category">
+                    <img src={nuts} alt="category-nuts" />
                     <h5>Nuts</h5>
                     
                 </div>
